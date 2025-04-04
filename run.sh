0e5749cf94ce6f538c83823ff0c5846ebc891719
Bruno Reis <bruno.queirozrq@gmail.com>
2025-04-03 22:19:17 -0300

    Corrige a localizacao de compilacao do projeto

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ rm -rf build
 
 mkdir -p build
 
-find src/main/java/com/flightbooking -name "*.java" -type f | xargs javac -verbose -d build
+find src -name "*.java" -type f | xargs javac -verbose -d build
 
 if [ -f "build/com/flightbooking/Main.class" ]; then
     echo "Main.class foi gerado com sucesso!"
